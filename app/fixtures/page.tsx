3eea002ad4682ad2424c8e251cfe4f06f2adf475
kwan <kwan@footfallcam.com>
2023-11-09 12:25:10 -0600

    First initial commit.

--- app/fixtures/page.tsx
+++ app/fixtures/page.tsx
@@ -18,6 +18,53 @@ import { sportState } from "@/components/atoms";
 const teams = ["UW-Madison", "UW-Milwaukee", "UW-Whitewater", "UW-Oshkosh"];
 const scores = ["11-12", "10-13", "12-11", "13-10"];
 const index = ["1", "2", "3", "4"];
+
+// create a dummy data for match details
+// [[team1, team2, date, time, venue]]
+// team1 is string, team2 is string, date should be in Nov 1, time should be in 8:00 AM
+// everything in stringx
+const matchDetails = {
+  Football: [
+    ["MySA Badgers FC", "PMX FC", "Nov 11", "9:00 AM", "Group"],
+    ["Illinois", "NSP", "Nov 11", "9:40 AM", "Group"],
+    ["Ola Bola Squad", "Gophers Cyclones FC", "Nov 11", "10:20 AM", "Group"],
+    ["MySA Badgers FC", "Illinois", "Nov 11", "11:00 AM", "Group"],
+    ["PMX FC", "NSP", "Nov 11", "11:40 AM", "Group"],
+    ["Ola Bola Squad", "Default", "Nov 11", "12:20 PM", "Group"],
+    ["MySA Badgers FC", "NSP", "Nov 11", "1:00 PM", "Group"],
+    ["PMX FC", "Illinois", "Nov 11", "1:40 PM", "Group"],
+    ["Gophers Cyclones FC", "Default", "Nov 11", "2:20 PM", "Group"],
+    ["A1", "B2", "Nov 11", "3:00 PM", "Playoffs"],
+    ["B1", "A2", "Nov 11", "3:40 PM", "Playoffs"],
+    ["TBD", "TBD", "Nov 11", "4:20 PM", "3rd & 4th"],
+    ["TBD", "TBD", "Nov 11", "5:00 PM", "Finals"],
+  ],
+  Volleyball: [
+    ["OSU Buckeyes", "Illinois", "Nov 11", "12:00 PM", "Court 5"],
+    ["Free Agent 1", "Free Agent 2", "Nov 11", "12:00 PM", "Court 6"],
+    ["Cendol VB", "Illinois", "Nov 11", "12:40 PM", "Court 5"],
+    ["OSU Buckeyes", "Free Agent 2", "Nov 11", "12:40 PM", "Court 6"],
+    ["Free Agent 2", "Cendol VB", "Nov 11", "1:20 PM", "Court 5"],
+    ["OSU Buckeyes", "Free Agent 1", "Nov 11", "1:20 PM", "Court 6"],
+    ["Free Agent 1", "Illinois", "Nov 11", "2:00 PM", "Court 5"],
+    ["Cendol VB", "OSU Buckeyes", "Nov 11", "5:30 PM", "Court 5"],
+    ["Free Agent 1", "Cendol VB", "Nov 11", "6:10 PM", "Court 5"],
+    ["Illinois", "Free Agent 2", "Nov 11", "6:10 PM", "Court 6"],
+  ],
+  Badminton: [
+    ["aaa", "bbb", "Nov 1", "8:00 AM", "Court A"],
+    ["ccc", "ddd", "Nov 1", "8:00 AM", "Court A"],
+  ],
+  Basketball: [
+    ["aaa", "bbb", "Nov 1", "8:00 AM", "Court A"],
+    ["ccc", "ddd", "Nov 1", "8:00 AM", "Court A"],
+  ],
+  Sukaneka: [
+    ["aaa", "bbb", "Nov 1", "8:00 AM", "Court A"],
+    ["ccc", "ddd", "Nov 1", "8:00 AM", "Court A"],
+  ],
+};
+
 export default function Fixture() {
   const [sport, setSport] = useRecoilState(sportState);
   return (
@@ -39,20 +86,39 @@ export default function Fixture() {
       <h1 className="text-4xl font-extrabold tracking-tight scroll-m-20 lg:text-5xl">
         {sport}
       </h1>
-      <Tabs defaultValue="account" className="w-[320px] justify-center">
-        <TabsList className="grid w-[320px] grid-cols-2 ">
+      <Tabs defaultValue="Matches" className="w-[320px] justify-center">
+        <TabsList className="grid w-[320px] grid-cols-2 mb-4 ">
           <TabsTrigger value="Matches">Matches</TabsTrigger>
           <TabsTrigger value="Standings">Standings</TabsTrigger>
         </TabsList>
         <TabsContent value="Matches">
-          Make changes to your account here.
+          {/* loop through matchDetails */}
+          {matchDetails[sport].map((matchDetail, index) => (
+            <Matches
+              className="duration-100 fade-in-10"
+              key={index}
+              team1={matchDetail[0]}
+              team2={matchDetail[1]}
+              date={matchDetail[2]}
+              time={matchDetail[3]}
+              venue={matchDetail[4]}
+            />
+          ))}
         </TabsContent>
-        <TabsContent value="Standings">Change your password here.</TabsContent>
+        <TabsContent value="Standings">
+          {" "}
+          <Standings
+            title="Group B"
+            teams={teams}
+            scores={scores}
+            nums={index}
+          />
+          <Brackets data={walkOverData} />
+        </TabsContent>
       </Tabs>
 
-      <Matches team1="aaa" team2="bbb" date="Nov 1" time="8:00 AM" />
-      <Standings title="Group B" teams={teams} scores={scores} nums={index} />
-      <Brackets data={walkOverData} />
+      {/* loop through matchDetails and output Matches component */}
+
       <Footer />
     </main>
   );
--- components/Matches.tsx
+++ components/Matches.tsx
@@ -6,26 +6,31 @@ interface Props {
   team2: string;
   date: string;
   time: string;
+  venue: string;
+  score1?: string;
+  score2?: string;
 }
-export const Matches = ({ team1, team2, date, time }: Props) => {
+export const Matches = ({ team1, team2, date, time, venue }: Props) => {
   return (
-    <div className="relative p-4 border h-[90px] border-slate-400 items-center min-w-[320px] rounded-md grid grid-cols-3 gap-4">
+    <div className="relative p-4 border h-[90px] border-slate-400 items-center min-w-[320px] rounded-md grid grid-cols-3 gap-3 mb-4">
       {/* Team */}
 
-      <div className="flex flex-col gap-1 min-w-[140px] col-span-2">
+      <div className="flex flex-col gap-1 min-w-[140px] col-span-2 text-sm">
         <p>{team1}</p>
         <p>{team2}</p>
       </div>
       {/* Separator */}
       <Separator
         orientation="vertical"
-        className="absolute top-0 h-10 translate-y-6 right-28 bg-slate-500"
+        className="absolute top-0 h-10 translate-y-6 right-[114px] bg-slate-500"
       />
 
       {/* Date */}
-      <div className="flex flex-col items-center justify-center text-xs font-semibold text-center ">
-        <p>{date}</p>
-        <p>{time}</p>
+      <div className="flex flex-col items-center justify-center text-xs font-semibold text-center whitespace-nowrap">
+        <div>
+          {date}, {time}
+        </div>
+        <div>{venue}</div>
       </div>
     </div>
   );
