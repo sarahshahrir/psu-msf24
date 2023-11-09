f74ec1d71551042ef32b92b2ca0bf9496989d2dc
kwan <kwan@footfallcam.com>
2023-11-08 23:19:33 -0600

    Updated imports.

--- app/fixtures/page.tsx
+++ app/fixtures/page.tsx
@@ -4,10 +4,10 @@ import namsa from "@/public/namsa.jpeg";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import { Footer } from "@/components/footer";
-import { Matches } from "@/components/matches";
-import { Standings } from "@/components/standings";
-import { Brackets } from "@/components/brackets";
+import { Footer } from "@/components/Footer";
+import { Matches } from "@/components/Matches";
+import { Standings } from "@/components/Standings";
+import { Brackets } from "@/components/Brackets";
 import { walkOverData } from "@/components/dummy";
 
 const teams = ["UW-Madison", "UW-Milwaukee", "UW-Whitewater", "UW-Oshkosh"];
@@ -15,7 +15,7 @@ const scores = ["11-12", "10-13", "12-11", "13-10"];
 const index = ["1", "2", "3", "4"];
 export default function Fixture() {
   return (
-    <main className="flex min-h-screen flex-col items-center  md:p-24 px-4 gap-6 py-8 ">
+    <main className="flex flex-col items-center min-h-screen gap-6 px-4 py-8 md:p-24 ">
       <div className="flex items-center gap-2 translate-x-[-10px] translate-y-3">
         <Image src={mysa} alt="Picture of the author" width={60} height={60} />
         <h1 className="text-border-white text-border-size-1 text-black md:text-[100px] font-extrabold text-3xl">
